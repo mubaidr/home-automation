46049731fc55f0b026cf29ec76d243d6763fcfee
Muhammad Ubaid Raza <mubaidr@gmail.com>
2019-04-20 22:08:51 +0500

    init

--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "./registerServiceWorker";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import './registerServiceWorker'
 
-Vue.config.productionTip = false;
+import 'material-design-icons/iconfont/material-icons.css'
+import './assets/main.sass'
+
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+Vue.use(Buefy, {
+  defaultTooltipAnimated: true
+})
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
